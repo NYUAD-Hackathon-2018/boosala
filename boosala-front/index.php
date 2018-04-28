7db1bd0e3b8c4456bb07c2a2bc75d3c34fe0e517
Said <saidziani.pro@gmail.com>
2018-04-28 22:48:23 +0400

    Adding result section.

--- boosala-front/index.php
+++ boosala-front/index.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-    <meta name="description" content="TWSS. est une équipe créative et dynamique qui vous propose la création de sites internet et une multitude de services utiles au développement de votre projet."/>
-    <meta name="keywords" content="thewebsitesquad, twss, création, design, développemet, algérie, alger, wordpress, cms, squad"> 
-    <title>BOOSALA | Bring home the lost</title>
+    <meta name="description" content="Bring home the lost"/>
+    <meta name="keywords" content="lost, missed, person"> 
+    <title>Boosala | Bring home the lost</title>
     <link rel="icon" href="images/logos/favicon.ico">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
@@ -17,7 +17,7 @@
 <body>
     <section id="home">
         <div class="container">
-            <div class="col-lg-6 col-md-6 col-xs-12 align-center">
+            <div id="logo-div" class="col-lg-6 col-md-6 col-xs-12 align-left">
                 <img id="logo" src="images/logo.png" />
             </div>
             <div id="main" class="col-lg-6 col-md-6 col-xs-12 align-right vertical-center">
@@ -29,71 +29,56 @@
     </section>
 
     <section id="search">
-        <a id="back">Back</a>        
+        <a id="back"><img src="images/arrow-left.png" alt="go-back" /></a>        
         <div class="container">
             <div class="col-lg-8 col-lg-offset-2 col-md-6 col-xs-12 align-center">
                 <img id="logo" src="images/logo.png" />
-                <h5>My name is <input type="text" name="me" placeholder="Enter your name"> and
+                <h5>My name is <input type="text" name="me" placeholder="Your name"> and
                 <br/>
-                I'm looking for <input type="text" name="him" placeholder="Enter Their name">.</h5>
+                I'm looking for <input type="text" name="him" placeholder="Their name">.</h5>
                 <div id="secondary" class="col-lg-6 col-md-6 col-xs-12 align-right">
-                <form enctype="multipart/form-data" action="index.php" method="POST">
-                    <label for="file-upload" >
-                        <img id="picture" src="images/face.jpeg" alt="Their"/>
-                    </label>
-                    <input id="file-upload" type='file' name="uploaded_file" onchange="readURL(this);" />
-                    </div>
-                    <div id="send" class="col-lg-6 col-md-6 col-xs-12 align-left">
-                        <button id="submit" name="submit">Submit My Information</button>
-                    </div>
-                </form>
+                    <!--index.php -->
+                    <!-- <form enctype="multipart/form-data" action="" method="POST">  -->
+                        <label for="file-upload">
+                            <img id="picture" src="images/face.jpeg" alt="Their"/>
+                        </label>
+                        <input id="file-upload" type='file' name="uploaded_file" onchange="readURL(this);" />
+                </div>
+                        <div id="send" class="col-lg-6 col-md-6 col-xs-12 align-left">
+                            <button id="submit" name="submit">Submit My Information</button>
+                        </div>
+                    <!-- </form> -->
                 <?PHP
-                  if(!empty($_FILES['uploaded_file'])){
-                    $path = "images/uploads/";
-                    $path = $path . basename( $_FILES['uploaded_file']['name']);
-                    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-                      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
-                      " has been uploaded";
-                    }else{
-                        echo "";
-                    }
-                  }
+                  // if(!empty($_FILES['uploaded_file'])){
+                  //   $path = "images/uploads/";
+                  //   $path = $path . basename( $_FILES['uploaded_file']['name']);
+                  //   if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+                  //     echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+                  //     " has been uploaded";
+                  //   }else{
+                  //       echo "";
+                  //   }
+                  // }
                 ?>
+            </div>
             </div>
         </div>
     </section>
 
-    <section id="search">
-        <a id="back">Back</a>        
+    <section id="result">
+        <a id="back">X</a>        
         <div class="container">
             <div class="col-lg-8 col-lg-offset-2 col-md-6 col-xs-12 align-center">
                 <img id="logo" src="images/logo.png" />
-                <h5>My name is <input type="text" name="me" placeholder="Enter your name"> and
-                <br/>
-                I'm looking for <input type="text" name="him" placeholder="Enter Their name">.</h5>
-                <div class="col-lg-6 col-md-6 col-xs-12 align-right">
-                <form enctype="multipart/form-data" action="index.php" method="POST">
-                    <label for="file-upload" >
-                        <img id="picture" src="images/face.jpeg" alt="Their"/>
-                    </label>
-                    <input id="file-upload" type='file' name="uploaded_file" onchange="readURL(this);" />
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-xs-12 align-left">
-                        <button id="submit" name="submit">Submit My Information</button>
-                    </div>
-                </form>
-                <?PHP
-                  if(!empty($_FILES['uploaded_file'])){
-                    $path = "images/uploads/";
-                    $path = $path . basename( $_FILES['uploaded_file']['name']);
-                    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-                      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
-                      " has been uploaded";
-                    }else{
-                        echo "There was an error uploading the file, please try again!";
-                    }
-                  }
-                ?>
+                <h5>FOUND</h5>
+                <h4>John may have been located at Damas on Monday, 29 March 2017</h4>
+                <iframe
+                  width="450"
+                  height="300"
+                  frameborder="0" style="border:0"
+                  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBAoGZUjYuhCmqwNIJH3FhoSTufZ-wjC6U
+                    &q=Space+Needle,Seattle+WA" allowfullscreen>
+                </iframe>
             </div>
         </div>
     </section>
